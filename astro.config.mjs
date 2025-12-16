e7cde0cb6059ba61259ffb4c84943b2c8586c349
Silver Ling <silver.ling@outlook.com>
2025-12-16 16:30:00 +0800

    add github pages ci/cd

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://async-event.github.io",
   vite: {
     plugins: [tailwindcss()],
   },
